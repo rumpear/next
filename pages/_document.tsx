8b188426b7024cf8573ea70267c6ef8d406cbb3d
rumpear <roman.f.grusha@gmail.com>
2023-01-31 17:14:43 +0200

    Document refactoring

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,18 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com'></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='anonymous'
-        ></link>
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
-          href='https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;700&family=Poppins:wght@100;300;500;700&display=swap'
-          rel='stylesheet'
-        ></link>
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;700&family=Poppins:wght@100;300;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
