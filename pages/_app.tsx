0321dd7ced4bdd07203c0cd1441908ce638a1d0f
rumpear <roman.f.grusha@gmail.com>
2023-01-31 17:14:10 +0200

    Head added to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <>
+      <Head>
+        <title>App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
