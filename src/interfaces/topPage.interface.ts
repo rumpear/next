312ac93f3d832aefb93a136fe31c8591b70c1a38
rumpear <roman.f.grusha@gmail.com>
2023-02-03 15:37:28 +0200

    TopPage interfaces added

---
+++ src/interfaces/topPage.interface.ts
@@ -0,0 +1,72 @@
+export enum LevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+
+export interface IAdvantage {
+  title: string;
+  description: string;
+  _id: string;
+}
+
+export interface IHhData {
+  count: number;
+  juniorSalary: number;
+  middleSalary: number;
+  seniorSalary: number;
+  updatedAt: Date;
+  _id: string;
+}
+
+export interface IBlog {
+  h1: string;
+  metaTitle: string;
+  metaDescription: string;
+  views: number;
+  _id: string;
+}
+
+export interface ISravnikus {
+  metaTitle: string;
+  metaDescription: string;
+  qas: unknown[];
+  _id: string;
+}
+
+export interface ILearningClub {
+  metaTitle: string;
+  metaDescription: string;
+  qas: unknown[];
+  _id: string;
+}
+
+export interface ITop {
+  _id: string;
+  tags: string[];
+  secondCategory: string;
+  alias: string;
+  title: string;
+  category: string;
+  seoText: string;
+  tagsTitle: string;
+  metaTitle: string;
+  metaDescription: string;
+  firstCategory: LevelCategory;
+  advantages: IAdvantage[];
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  hh: IHhData;
+  qas: unknown[];
+  addresses: unknown[];
+  categoryOn: string;
+  blog: IBlog;
+  sravnikus: ISravnikus;
+  learningclub: ILearningClub;
+}
+
+export interface ITopRes {
+  data: ITop[];
+}
