c7d6086fa1eb08dd0b3d250eb27f72a936d6cf77
rumpear <roman.f.grusha@gmail.com>
2023-02-10 17:59:09 +0200

    IMenu added

--- pages/comments/[id].tsx
+++ pages/comments/[id].tsx
@@ -1,14 +1,17 @@
+import axios from 'axios';
 import { GetServerSideProps } from 'next';
 import { CommentDetails } from '../../src/components';
+import { IMenu } from '../../src/interfaces/menu.interface';
 
-export default function Comment({ data }: any) {
+export default function Comment({ data, menu }: any) {
+  console.log(menu);
   return (
-    <div className='Comment'>
+    <div className="Comment">
       <p>{data?.name}</p>
       <p>{data?.body}</p>
       <p>
         <span>Author: </span>
-        <a href={`mailto:${data?.email}`} type='email'>
+        <a href={`mailto:${data?.email}`} type="email">
           {data?.email}
         </a>
       </p>
@@ -16,15 +19,19 @@ export default function Comment({ data }: any) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async context => {
   const { id } = context.params!;
+  const { data: menu } = await axios.get<IMenu[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + 'api/top-page/byAlias/' + 'photoshop',
+    // { firstCategory: FIRST_CATEGORY },
+  );
 
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/comments/${id}`
+    `https://jsonplaceholder.typicode.com/comments/${id}`,
   );
   const data = await res.json();
 
   return {
-    props: { data },
+    props: { data, menu },
   };
 };
