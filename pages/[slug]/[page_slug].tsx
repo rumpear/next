d59711951e20c922f5f06b0f97fa3cafc70b33ed
rumpear <roman.f.grusha@gmail.com>
2023-02-10 17:58:00 +0200

    page_slug added

---
+++ pages/[slug]/[page_slug].tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const Test = () => {
+  //   console.log(activeCategory, 'activeCategory');
+  //   console.log(menu, 'menu');
+  // console.log(categoriesList, 'categoriesList');
+
+  return <h1>Menu page</h1>;
+};
+
+export default Test;
